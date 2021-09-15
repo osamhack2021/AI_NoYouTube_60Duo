4c1bb8989be295b19eff4117c7f7fa02109b20f6
guitarzoa <87269633+guitarzoa@users.noreply.github.com>
2021-09-15 13:52:37 +0000

    Push

---
+++ AI/test_1.py
@@ -0,0 +1,11 @@
+# 현재창 정보 가져오기
+import win32gui
+import time
+
+window = ''
+while True:
+    current_window = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+    if window != current_window:
+        window = current_window
+        print(window)
+    time.sleep(1)
