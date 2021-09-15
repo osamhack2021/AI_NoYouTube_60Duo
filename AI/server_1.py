da7a8e8fac59ba91904aa22843d3a185cf118659
guitarzoa <87269633+guitarzoa@users.noreply.github.com>
2021-09-15 14:50:37 +0000

    commit on AI

---
+++ AI/server_1.py
@@ -0,0 +1,11 @@
+# python -m pip install flask-ngrok
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route("/")
+def home():
+    return "<h1>This is your Flask server.</h1>"
+app.run()
