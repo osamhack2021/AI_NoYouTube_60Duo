23ab767ac022b99a89cd32ac1fa1002f059f3efd
guitarzoa <87269633+guitarzoa@users.noreply.github.com>
2021-10-11 14:22:25 +0000

    new_ML

---
+++ AI/ML_3.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+from tqdm import tqdm
+from konlpy.tag import Okt
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+
+okt = Okt()
+
+file_name = 'tab_text_dataset.csv'
+data = pd.read_csv(file_name, encoding=('cp949'), names=['tab','study'])
+
+data.drop_duplicates(subset=['tab'], inplace=True) # 데이터 중복 제거
+
+# 영어,한글, 공백 남기고 제거
+X_data = np.array(data['tab'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣A-Za-z0-9 ]",""))
+y_data = np.array(data['study'])
+
+#불용어 목록
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로',
+             '자','에','와','한','하다','YouTube', '검색', '네이버', 'Google',
+             'Yahoo']
+
+X_train = []
+for sentence in tqdm(X_data):
+    tokenized_sentence = okt.morphs(sentence, stem=True) # 토큰화
+    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stopwords] # 불용어 제거
+    X_train.append(stopwords_removed_sentence)
+    
+# 정수인코딩
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+
+# 빈도수 낮은 단어 검색
+threshold = 3
+total_cnt = len(tokenizer.word_index) # 단어의 수
+rare_cnt = 0 # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+total_freq = 0 # 훈련 데이터의 전체 단어 빈도수 총 합
+rare_freq = 0 # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+for key, value in tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어의 등장 빈도수가 threshold보다 작으면
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+print('단어 집합(vocabulary)의 크기 :',total_cnt)
+print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s'%(threshold - 1, rare_cnt))
+print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt)*100)
+print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq)*100)
+
+# 전체 단어 개수 중 빈도수 2이하인 단어는 제거.
+# 0번 패딩 토큰을 고려하여 + 1
+vocab_size = total_cnt - rare_cnt + 1
+print('단어 집합의 크기 :',vocab_size)
+
+# 단어 정수화
+tokenizer = Tokenizer(vocab_size) 
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+print(X_train[:3])
+
+# 빈 샘플들을 제거
+drop_train = [index for index, sentence in enumerate(X_train) if len(sentence) < 1]
+X_train = np.delete(X_train, drop_train, axis=0)
+y_train = np.delete(y_data, drop_train, axis=0)
+
+
+print('리뷰의 최대 길이 :',max(len(l) for l in X_train))
+print('리뷰의 평균 길이 :',sum(map(len, X_train))/len(X_train))
+plt.hist([len(s) for s in X_train], bins=50)
+plt.xlabel('length of samples')
+plt.ylabel('number of samples')
+plt.show()
+
+
+# 데이터 길이 맞추기 패딩 
+def below_threshold_len(max_len, nested_list):
+  cnt = 0
+  for s in nested_list:
+    if(len(s) <= max_len):
+        cnt = cnt + 1
+  print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s'%(max_len, (cnt / len(nested_list))*100))
+
+max_len = 20
+below_threshold_len(max_len, X_train)
+
+X_train = pad_sequences(X_train, maxlen = max_len)
+
+
+
+# 데이터셋 나누기
+X_train, X_test, y_train, y_test = train_test_split(X_train, 
+                                                    y_train, 
+                                                    test_size=0.2, 
+                                                    shuffle=True, 
+                                                    random_state=64)
+
+if __name__ == '__main__':
+    from tensorflow.keras.layers import Embedding, Dense, LSTM
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.models import load_model
+    from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+    
+    model = Sequential()
+    model.add(Embedding(vocab_size, 100))
+    model.add(LSTM(128))
+    model.add(Dense(1, activation='sigmoid'))
+    
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+    mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+    
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+    history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=60, validation_split=0.2)
+    
+    loaded_model = load_model('best_model.h5')
+    print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+    
+    
+    def sentiment_predict(new_sentence):
+      new_sentence = okt.morphs(new_sentence, stem=True) # 토큰화
+      new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
+      encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
+      pad_new = pad_sequences(encoded, maxlen = max_len) # 패딩
+      score = float(loaded_model.predict(pad_new)) # 예측
+      if(score > 0.5):
+        print("{:.2f}% 확률로 공부중입니다.\n".format(score * 100))
+      else:
+        print("{:.2f}% 확률로 딴짓중 입니다..\n".format((1 - score) * 100))
+        
+    sentiment_predict("영어회화를 건강하게! 영어공부 필수 비타민 #4 I'm here to~ 영어회화학원영어발음 영어책추천 - YouTube")
